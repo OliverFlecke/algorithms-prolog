c1c67a922abd587b715670189805aae96bcd4cc7
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-30 09:34:31 +0800

    Reading sudoku problems from file

--- src/sudoku.pl
+++ src/sudoku.pl
@@ -1,3 +1,17 @@
+/**
+* Program to solve Sudokus using Prolog.
+* the sudoku/1 predicate takes a list of list (Rows)
+* and find the valid solutions for the given problem.
+* See 'samples/sudoku/' for examples.
+*
+* Use the read_problem/2 utility to read in a problem from a
+* text file.
+* Example:
+* 	read_problem('samples/sudoku/1.txt', P), sudoku(P), pretty(P).
+*
+* pretty/1 will output the solution with each row on its own line.
+*/
+
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
@@ -15,10 +29,15 @@ sudoku(Rows) :-
 	check_square(G, H, I).
 
 check_square([], [], []).
-check_square([N1, N2, N3 | Tail1], [N4, N5, N6 | Tail2], [N7, N8, N9 | Tail3]) :-
-	all_distinct([N1, N2, N3, N4, N5, N6, N7, N8, N9]),
+check_square([N11, N12, N13 | Tail1], [N21, N22, N23 | Tail2], [N31, N32, N33 | Tail3]) :-
+	all_distinct([N11, N12, N13, N21, N22, N23, N31, N32, N33]),
 	check_square(Tail1, Tail2, Tail3).
 
 pretty(Solution) :-
 	maplist(labeling([ff]), Solution),
 	maplist(portray_clause, Solution).
+
+read_problem(Filename, Problem) :-
+	open(Filename, read, Str),
+	read(Str, Problem),
+	close(Str).
